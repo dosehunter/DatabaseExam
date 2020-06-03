d5822038cbd6dbdb77514847abfd9530efad5cf8
dosehunter <arvidanker95@gmail.com>
2020-06-03 12:19:27 +0200

    Added unique exam_grades and comments

--- school_setup.sql
+++ school_setup.sql
@@ -5,9 +5,13 @@ USE school;
 ##################################################
 -- DATABASE SETUP
 ##################################################
--- TABLES
-##################################################
 
+##################################################
+################### DEPENDENT ####################    
+-- The responsible dependent/s for a student
+-- Name, phone, address and email will be stored
+-- So that a teacher can contact them should anything happen
+##################################################  
 CREATE TABLE dependent(
 	dep_id INT
 		NOT NULL
@@ -17,8 +21,12 @@ CREATE TABLE dependent(
     address VARCHAR(30),
     email VARCHAR(30), 
     PRIMARY KEY (dep_id));
-    
--- Teacher/employee
+
+##################################################
+#################### EMPLOYEE ####################    
+-- Holds name, date of birth, cpr, gender and salary
+################################################## 
+   
 CREATE TABLE employee(
 	id INT
 		NOT NULL
@@ -30,6 +38,12 @@ CREATE TABLE employee(
     salary INT, #(6,2),
     PRIMARY KEY (id));  
     
+##################################################  
+##################### CLASS ######################  
+-- Students are assigned a primary class
+-- Needs info on class year, expected finish date, level, and headteacher
+##################################################    
+
 CREATE TABLE class(
 	id INT
 		NOT NULL
@@ -43,7 +57,14 @@ CREATE TABLE class(
 			ON UPDATE CASCADE 
             ON DELETE SET NULL,  -- If a teacher leaves the institution, the class might temporarily miss a head teacher
 	PRIMARY KEY (id));
-    
+
+##################################################    
+#################### STUDENT #####################
+-- Student table holds necessary info of students
+-- name, date of birth, cpr, gender, email, phone, and class number
+-- A student can be assigned a maximum of one class
+##################################################
+
 CREATE TABLE student(
 	id INT
 		NOT NULL
@@ -60,10 +81,18 @@ CREATE TABLE student(
 			ON UPDATE CASCADE
             ON DELETE SET NULL,
     PRIMARY KEY (id));
-    
+
+################################################## 
+################### STUD_DEP #####################   
+-- Bridge table between student and dependent
+-- Combination of FK's must be unique
+##################################################
+
 CREATE TABLE stud_dep(
-	student_id INT NOT NULL,
-    dependent_id INT NOT NULL,
+	student_id INT 
+		NOT NULL,
+    dependent_id INT 
+		NOT NULL,
     FOREIGN KEY (student_id) 
 		REFERENCES student(id) 
 			ON UPDATE CASCADE 
@@ -74,7 +103,12 @@ CREATE TABLE stud_dep(
 			ON DELETE CASCADE,
 	UNIQUE KEY `unique_comb`(student_id, dependent_id),
 	PRIMARY KEY (student_id, dependent_id));
-    
+
+##################################################  
+#################### SUBJECT #####################  
+-- Constains subject name and subject level
+##################################################
+
 CREATE TABLE `subject`(
 	id INT
 		NOT NULL
@@ -83,25 +117,46 @@ CREATE TABLE `subject`(
     subject_level INT,
     PRIMARY KEY (id));    
  
+ ##################################################
+ ################## EXAM_GRADES ###################
+-- Exam grades contains student, a grade, a subject and a date of the exam
+-- Student id and subject id is the primary key, as a student should only 
+-- have one exam grade for a subject.
+##################################################
+ 
 CREATE TABLE exam_grades(
-	student_id INT,
-    `subject_id` INT,
+	student_id INT
+		NOT NULL,
+    subject_id INT
+		NOT NULL,
     grade INT,
     exam_date DATE,
     FOREIGN KEY (student_id) 
 		REFERENCES student(id) 
 			ON UPDATE CASCADE 
-            ON DELETE restrict,
-	FOREIGN KEY (`subject_id`) 
+            ON DELETE RESTRICT,
+	FOREIGN KEY (subject_id) 
 		REFERENCES `subject`(id) 
 			ON UPDATE CASCADE 
             ON DELETE RESTRICT,
-	PRIMARY KEY (student_id, `subject_id`));
+	UNIQUE KEY `exam_unq_comb`(student_id, subject_id),
+	PRIMARY KEY (student_id, subject_id));
+
+##################################################
+###################### ROOM ######################
+-- Room table holds room_number and amount of people it can hold at a given time
+##################################################
 
 CREATE TABLE room(
 	room_number INT,
 	size INT,
 	PRIMARY KEY (room_number));
+
+################################################## 
+################## TIME_TABLE ####################   
+-- Time table connects room, class, employee, and teacher together with a date and a time
+-- So that the school can when teachers teach and what rooms are occupied
+##################################################
 
 CREATE TABLE time_table(
 	id INT 
@@ -131,9 +186,16 @@ CREATE TABLE time_table(
             ON DELETE SET NULL,
 	PRIMARY KEY (id));
 
+##################################################
+################### CAN_TEACH ####################    
+-- Bridge table between employee (teacher) and subjects he/she is able to teach
+##################################################
+
 CREATE TABLE can_teach(
-	teacher_id INT NOT NULL,
-    `subject_id` INT NOT NULL,
+	teacher_id INT 
+		NOT NULL,
+    `subject_id` INT 
+		NOT NULL,
     FOREIGN KEY (teacher_id)
 		REFERENCES employee(id)
 			ON UPDATE CASCADE
@@ -145,16 +207,20 @@ CREATE TABLE can_teach(
 	UNIQUE KEY `unique_comb_teach`(teacher_id, subject_id),
 	PRIMARY KEY (teacher_id, `subject_id`));
 
-##################################################    
+##################################################
 -- CREATING EXTRA INDEXES
 ##################################################
-
 -- As we're using these to join tables we have chosen to create indexes for them
-CREATE INDEX idx_emp_name ON employee(full_name);
-CREATE INDEX idx_dep_name ON dependent(full_name);
+
+CREATE INDEX idx_emp_name 
+	ON employee(full_name);
+    
+CREATE INDEX idx_dep_name 
+	ON dependent(full_name);
 
 ##################################################    
 -- REQUIREMENTS (TRIGGERS/EVENTS) 
 ##################################################
 # NOTE TRIGGERS SHOULD BE RUN AFTER POPULATING THE DATABASE!
+# AND CAN BE FOUND IN "Trigger.sql"
 
